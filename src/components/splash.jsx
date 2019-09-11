604d4758c24b755406d2b7e1b259ae42d49423d4
jesusmontano <jesusmontano96@gmail.com>
2019-09-11 13:15:18 -0700

    Adjust splash

--- src/components/splash.jsx
+++ src/components/splash.jsx
@@ -6,12 +6,16 @@ const Splash = () => {
     return (
         <div className="splash">
             <div className="get-started">
-                <div>
+                <div className="tagline">
                     Shape Your Financial Future!
                 </div>
                 <br/>
+                <div>
+                    Adjust Your Portfolio at JMCP
+                </div>
+                <br/>
                 <button>
-                    <Link to='/risk' style={{ textDecoration: 'none', color: 'black' }}>Get Started</Link>
+                    <Link to='/risk' style={{ textDecoration: 'none', color: 'black' }}>Get Started!</Link>
                 </button>
             </div>
         </div>
